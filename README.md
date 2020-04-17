86a62ed1792e3730723c9c3a82057c8ddab063c8
peto levente <levente.peto@aimotive.com>
2020-04-17 14:39:17 +0200

    readme setup update

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-utils
+Jarvis utils
+
+# Install
+~~~ shell
+pip3 install git+https://github.com/levopeti/utils.git
+~~~
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='jarvis',
     version='0.0.1',
     description='Utils for general tasks.',
-    url='https://github.com/levopeti/utils.git',
+    url='git@github.com:levopeti/utils.git',
     author='Levente Peto',
     author_email='leviskocka@gmail.com',
     license='unlicense',
