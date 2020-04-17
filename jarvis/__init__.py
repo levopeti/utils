12294a788b68ad23174e774a9d888290899dd83d
peto levente <levente.peto@aimotive.com>
2020-04-17 14:42:36 +0200

    add profiler

--- jarvis/__init__.py
+++ jarvis/__init__.py
@@ -1,3 +1,4 @@
 from jarvis_print import jprint
 from jarvis_utils import jtimer, make_index_chunks, new_thread
 from nvidia_smi import NvidiaSmi
+from jarvis_profiler import Profiler
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='jarvis',
-    version='0.0.1',
+    version='0.0.2',
     description='Utils for general tasks.',
     url='git@github.com:levopeti/utils.git',
     author='Levente Peto',
