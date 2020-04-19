ec4c108b8329ab2ef2e73133aa27f2ca320124be
peto levente <levente.peto@aimotive.com>
2020-04-19 11:54:42 +0200

    update setup

--- jarvis/__init__.py
+++ jarvis/__init__.py
@@ -2,3 +2,4 @@ from jarvis.jarvis_print import jprint
 from jarvis.jarvis_utils import jtimer, make_index_chunks, new_thread
 # from jarvis.nvidia_smi import NvidiaSmi
 from jarvis.jarvis_profiler import Profiler
+from jarvis.jarvis_cache import cache
